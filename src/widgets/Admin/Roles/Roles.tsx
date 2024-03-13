2730882ea2c4536fd8094935f542252505c6b265
s3ABDI <abdirahmanjuma2008@gmail.com>
2024-03-13 13:20:20 +0600

    RolesList is done

--- src/widgets/Admin/Roles/Roles.tsx
+++ src/widgets/Admin/Roles/Roles.tsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
 import styles from "./Roles.module.scss";
+import RolesMenu from "./components/rolesMenu/RolesMenu";
+import RolesSettings from "./components/settings/RolesSettings";
 
 export const Roles = () => {
-    return <div className={styles.Roles}>Roles</div>;
+    const [settings, setSettings] = useState<boolean>(false)
+    const openSetting = () => {
+        setSettings(true)
+    }
+    return <div className={styles.Roles}>
+        {settings ? <RolesSettings/> : <RolesMenu openSettings={openSetting}/>}
+        
+    </div>;
 };
---
+++ src/widgets/Admin/Roles/components/rolesMenu/RolesList.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import styles from "./RolesList.module.scss"
+
+interface IRolesList{
+    list: object,
+    listType: string
+}
+
+const RolesList:React.FC<IRolesList> = ({list, listType}) => {
+   
+    return(
+        <div className={styles.MenuCont}>   
+        <div className={styles.HeaderPrg}>
+            <p>{listType}</p>
+        </div>
+        {/* <div className={styles.RenderCont}> */}
+            {
+                list.map((el) => {
+                    return <div className={styles.ListItem}>
+                        <input type="checkbox" /> <p>{el}</p>
+                    </div>
+                })
+            }
+        {/* </div> */}
+            
+        </div>
+    )
+}
+
+export default RolesList
