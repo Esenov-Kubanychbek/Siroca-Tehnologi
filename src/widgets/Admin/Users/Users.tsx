17b63880d55c9e1c0a22ed3433d10ce9615a8cbe
DalerKulchukov <laxusdreyar20020709@gmail.com>
2024-03-13 12:18:22 +0600

    UsersList is done

--- src/widgets/Admin/Users/Users.tsx
+++ src/widgets/Admin/Users/Users.tsx
@@ -1,5 +1,23 @@
+import { FC } from "react";
 import styles from "./Users.module.scss";
+import { SearchInput } from './../../../features/Header/SearchInput/SearchInput';
+import { ButtonCreate } from "../../../shared/ui/ButtonCreate/ButtonCreate";
+import { UsersList } from "./ui/UsersList";
 
-export const Users = () => {
-    return <div className={styles.Users}>Users</div>;
+
+export const Users:FC = () => {
+    return (
+        <div className={styles.UsersComponent}>
+            <div className={styles.UsersSearch}>
+            <div>Поиск по пользователям</div>
+                <div className={styles.Search}>
+                    <SearchInput/>
+                    <ButtonCreate name="Создать пользователя"/>
+                </div>
+            </div>
+            <div className={styles.UserList}>
+                <UsersList/>
+            </div>
+        </div>
+    );
 };
